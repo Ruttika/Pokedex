6a8a002b22e713106ea8992fb24fe56665eb2f5c
Rudolf Dani <ruttika@Rudolfs-iMac.local>
2018-09-11 23:43:23 +0200

    before Alamo

--- Pokedex/Controller/PokemonDetailVC.swift
+++ Pokedex/Controller/PokemonDetailVC.swift
@@ -10,7 +10,24 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
- 
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokeIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currenEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    @IBAction func backBtn(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
@@ -19,6 +36,7 @@ class PokemonDetailVC: UIViewController {
        
     }
 
+    
     
 
 }
