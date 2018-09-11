e46ea6b86b0dff52e222428fd012bca08a1f1f0b
Rudolf Dani <ruttika@Rudolfs-iMac.local>
2018-09-11 20:41:15 +0200

    details UI without stackView

---
+++ Pokedex/Controller/PokemonDetailVC.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Rudolf Dani on 2018. 09. 11..
+//  Copyright © 2018. Rudolf Dani. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+ 
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    
+
+}
